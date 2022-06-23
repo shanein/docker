7a2b6d46c325a0145880d7732683d6d87cb645f4
samuel.hanein <samuel.hanein@eemi.com>
2022-06-24 00:27:28 +0200

    add django node and golang

---
+++ docker-golang/database/migration.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `users`
+(
+    id   bigint auto_increment,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+INSERT INTO `users` (`name`)
+VALUES ('Solomon'),
+       ('Raphael'),
+       ('Menelik');
